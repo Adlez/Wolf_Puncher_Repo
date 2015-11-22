37be555435ddb784b19b7a355d843ce04a7674ef
Adlez <srgnt_booyaw@live.ca>
2015-11-22 14:55:52 -0500

    New Mayor Object.
    New mayor does not rise for no reason
    Also does not jump or punch yet

---
+++ Assets/Scripts/MayorMovement.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MayorMovement : MonoBehaviour 
+{
+    public GameObject m_MayorController;
+
+    public Vector3 m_Velocity;
+    public Vector3 m_Rotation;
+    public Vector3 m_Scaling;
+
+    public Animator m_Animator;
+    public Animator m_IritAnimator;
+    public Animator m_SadAnimator;
+    public Animator m_BoredAnimator;
+
+    public bool m_FacingRight;
+    bool onGround_;
+
+    public float m_JumpForce;
+
+    public bool OnGround
+    {
+        get
+        {
+            return onGround_;
+        }
+        set
+        {
+            onGround_ = value;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var Mayor = m_MayorController.GetComponent<MayorStats>();
+        if (collision.collider.tag == "Ground")
+        {
+            onGround_ = true;
+            m_Animator.SetBool("IsJumping", false);
+        }
+
+        if (collision.collider.tag == "Wolf")
+        {
+            Mayor.m_WolvesKilled++;
+
+            Mayor.m_tempMad += 0.5f;
+            m_IritAnimator.SetFloat("Irritation", Mayor.m_tempMad);
+        }
+        if (collision.collider.tag == "Bunny")
+        {
+            Mayor.m_tempSad += 1;
+            m_SadAnimator.SetInteger("Sadness", Mayor.m_tempSad);
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        Debug.Log("OnTriggerStay");
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        Debug.Log("OnTriggerExit");
+    }
+
+    void FixedUpdate()
+    {
+        if (onGround_ && Input.GetAxis("Jump") > 0)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * m_JumpForce);
+            m_Animator.SetBool("IsJumping", true);
+            onGround_ = false;
+        }
+    }
+}
---
+++ Assets/Scripts/MayorStats.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MayorStats : MonoBehaviour 
+{
+    public float mPunchRate;
+    public float Irritation;
+    public float Boredom;
+    public int Sadness;
+    public int m_WolvesKilled;
+
+    public float m_TimeToNextPunch;
+    public float m_tempMad;
+    public float m_tempBored;
+    public int m_tempSad;
+
+    //Constants to be relocated elsewhere
+    public const int MAXIRRITATION = 10;
+    public const int MAXSADNESS = 5;
+    public const float MAXBOREDOM = 60.0f;
+    
+}
